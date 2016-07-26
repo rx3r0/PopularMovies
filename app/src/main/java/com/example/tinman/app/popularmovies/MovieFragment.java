abcb011688561d5861d403f1d378bd638dbf819a
Martin Watson <martinwatsonja@gmail.com>
2016-07-25 22:03:05 -0500

    Remove old dummy data

--- app/src/main/java/com/example/tinman/app/popularmovies/MovieFragment.java
+++ app/src/main/java/com/example/tinman/app/popularmovies/MovieFragment.java
@@ -11,9 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
 /**
@@ -66,22 +64,6 @@ public class MovieFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_movie, container, false);
-
-        // Create dummy data for use with testing the view
-        String[] dummyData = {
-                "Batman v Superman: Dawn of Justice - 2016-03-23",
-                "The Legend of Tarzan - 2016-06-29",
-                "Independence Day: Resurgence - 2016-06-22",
-                "The Hunger Games: Mockingjay - Part 1 - 2014-11-18",
-                "The Boy Next Door - 2015-01-23",
-                "Finding Dory - 2016-06-16",
-                "Captain America: Civil War - 2016-04-27",
-                "Mad Max: Fury Road - 2015-05-13",
-                "Ghostbusters - 2016-07-14"
-        };
-
-        // Put dummy data in list
-        List<String> movieList = new ArrayList<>(Arrays.asList(dummyData));
 
         // Initialise the MovieInfoAdapter with the activity context which is the layout's
         // parent activity, and the movieInfo array list with the data.
